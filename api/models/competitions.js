5d5c74d556a05878ba1824eee5a3b9b38c0f59e4
cfsepulveda <cf.sepulveda@uniandes.edu.co>
2018-11-12 18:56:56 -0500

    fix

--- api/models/competitions.js
+++ api/models/competitions.js
@@ -118,17 +118,17 @@ exports.find = function (id, callback) {
     };
 
     mc.get(id, function (err, value, key) {
-            console.log("get");
-            console.log(value);
+        console.log("get");
+        console.log(value);
         if (value != null) {
             console.log("if");
-            callback({ competition: value });
+            callback({ competition: JSON.parse(value) });
         } else {
             docClient.query(params, function (err, data) {
                 if (err) {
                     callback({ code: 500 });
                 } else {
-                    mc.set(id, data.Items[0]);
+                    mc.set(id, JSON.stringify(data.Items[0]));
                     callback({ competition: data.Items[0] });
                 }
             });
