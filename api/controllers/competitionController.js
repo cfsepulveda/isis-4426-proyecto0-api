669b2f5247b96c8c6111206e68239ec7a7d1467d
Cristian Sepulveda <sepulvec@colpatria.com>
2018-11-14 20:50:22 -0500

    test

--- api/controllers/competitionController.js
+++ api/controllers/competitionController.js
@@ -15,7 +15,7 @@ exports.create = function (req, res) {
     ) {
         try{
                   if (!isNaN(insertedId)) {
-            res.sendStatus(201);
+            res.sendStatus(201).body({ ...req.body, id: insertedId });
             return;
         }
         if (insertedId && insertedId.message === "Error existing address") {
@@ -27,7 +27,7 @@ exports.create = function (req, res) {
             return;
         }
         }catch(error){
-            res.sendStatus(201);
+            res.sendStatus(201).body({ ...req.body, id: insertedId });
             return;
         }
   
